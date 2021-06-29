fae8911a25ffc5d4f9919164be8d11bee1ef96e5
Goossage <igorgooss@gmail.com>
2021-06-29 16:32:25 +0300

    work with admin

--- products/models.py
+++ products/models.py
@@ -5,6 +5,8 @@ from django.db import models
 class ProductCategory(models.Model):
     name = models.CharField(max_length=64, unique=True)
     description = models.TextField(blank=True)
+    def __str__(self):
+        return self.name
 
 class Product(models.Model):
     image = models.ImageField(upload_to="products_images", blank = True)
@@ -14,3 +16,6 @@ class Product(models.Model):
     price = models.DecimalField(max_digits=8, decimal_places=2, default=0)
     quantity = models.PositiveIntegerField(default=0)
     category = models.ForeignKey(ProductCategory, on_delete=models.PROTECT)
+
+    def __str__(self):
+        return f' {self.name} | {self.category}'
