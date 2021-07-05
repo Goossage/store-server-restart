3cb91f30fe721cd769a0c5380f391a0464396c5e
Goossage <igorgooss@gmail.com>
2021-07-05 17:02:42 +0300

    Механизм авторизации и аутентификации

--- users/views.py
+++ users/views.py
@@ -1,9 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponseRedirect
+from django.urls import reverse
+from django.contrib import auth
 
-# Create your views here.
+from users.forms import UserLoginForm
 
 def login(request):
-    return render(request, 'users/login.html')
+    if request.method == "POST":
+        login_form = UserLoginForm(data = request.POST)
+        print(login_form)
+        if login_form.is_valid():
+            username = request.POST['username']
+            password = request.POST['password']
+            user = auth.authenticate(username=username, password=password)
+            if user and user.is_active:
+                auth.login(request, user)
+                return HttpResponseRedirect(reverse('index'))
+    else:
+        login_form = UserLoginForm()
+    context = {
+        "login_form": login_form,
+    }
+    return render(request, 'users/login.html', context)
 
 def register(request):
     return render(request, 'users/register.html')
